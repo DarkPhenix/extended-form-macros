e7f9656942ad7541dccfdec27425d45f57f252e5
Richard Ejem <richard@ejem.cz>
2016-08-22 14:39:36 +0200

    fixed interface types and names

--- src/IExtendedFormRenderer.php
+++ src/IExtendedFormRenderer.php
@@ -6,12 +6,25 @@ use Nette\Forms\Container;
 use Nette\Forms\ControlGroup;
 use Nette\Forms\IControl;
 use Nette\Forms\IFormRenderer;
+use Nette\Utils\Html;
 
 interface IExtendedFormRenderer extends IFormRenderer
 {
+    /**
+     * @param IControl $control
+     * @return Html
+     */
     public function renderPair(IControl $control);
 
-    public function renderGroup(ControlGroup $control);
+    /**
+     * @param ControlGroup $group
+     * @return Html
+     */
+    public function renderGroup(ControlGroup $group);
 
-    public function renderContainer(Container $control);
+    /**
+     * @param Container $container
+     * @return Html
+     */
+    public function renderContainer(Container $container);
 }
